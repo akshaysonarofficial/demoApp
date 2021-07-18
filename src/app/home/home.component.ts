c98ff2b561d62ee78f65cb6d3392feaed9a1e3ba
akshaysonarofficial <akshaysonarofficial@gmail.com>
2021-07-18 20:31:27 +0530

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,6 +29,29 @@ export class HomeComponent implements OnInit {
 
 
   }
+  delete(id){
+
+    for(let i=0;i<this.data.length;i++){
+      if(id==this.data[i]){
+        this.data.splice(i,1);
+      }
+    }
+
+  }
+
+  update(info) {
+    this.regForm = new FormGroup({
+      fname: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(20), Validators.pattern('^[a-zA-Z ]*$')]),
+      lname: new FormControl(''),
+      email: new FormControl(''),
+      age: new FormControl(''),
+      state: new FormControl(''),
+      country: new FormControl(''),
+      address: new FormControl(''),
+      Tags: new FormControl(''),
+    });
+
+  }
   
    
   
